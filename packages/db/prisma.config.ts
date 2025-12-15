72fc054edd495fde1618909dfffe2ab82aeb4a7c
Rajdeep Das <rajdeepds626@gmail.com>
2025-12-15 14:36:27 +0000

    db: add flagbase prisma schema

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import dotenv from "dotenv";
 import type { PrismaConfig } from "prisma";
+import { env } from "prisma/config";
 
 dotenv.config({
   path: "../../apps/web/.env",
@@ -10,5 +11,8 @@ export default {
   schema: path.join("prisma", "schema"),
   migrations: {
     path: path.join("prisma", "migrations"),
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
   },
 } satisfies PrismaConfig;
