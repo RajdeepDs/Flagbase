9a1bdbe47256ddecb7dbb595be2c595f377ccc40
Rajdeep Das <rajdeepds626@gmail.com>
2026-01-01 07:04:07 +0000

    web: replace switch with label in flags columns

--- apps/web/src/components/flags/flags-columns.tsx
+++ apps/web/src/components/flags/flags-columns.tsx
@@ -2,15 +2,12 @@
 
 import type { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
-import { useState } from "react";
-import { Switch } from "../ui/switch";
 
 export interface Flag {
   id: string;
   name: string;
   key: string;
   status: Boolean;
-  default: Boolean;
 }
 
 export const columns: ColumnDef<Flag>[] = [
@@ -24,29 +21,11 @@ export const columns: ColumnDef<Flag>[] = [
     ),
   },
   {
-    accessorKey: "default",
-    header: "Default",
-    cell: ({ row }) => {
-      const isDefault = row.getValue("default") as boolean;
-      return <span>{isDefault ? "True" : "False"}</span>;
-    },
-  },
-  {
     accessorKey: "status",
     header: "Status",
     cell: ({ row }) => {
-      const [currentStatus, setCurrentStatus] = useState(
-        row.getValue("status") as boolean
-      );
-      return (
-        <Switch
-          checked={currentStatus}
-          onCheckedChange={(value) => {
-            setCurrentStatus(value);
-            console.log(`Flag ID: ${row.original.id}, New Status: ${value}`);
-          }}
-        />
-      );
+      const isDefault = row.getValue("status") as boolean;
+      return <span>{isDefault ? "Enabled" : "Disabled"}</span>;
     },
   },
 ];
