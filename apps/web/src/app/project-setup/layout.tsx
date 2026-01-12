679c477556f567611a867a95652cf9289721bf02
Rajdeep Das <rajdeepds626@gmail.com>
2026-01-12 07:22:54 +0000

    web: add ORPC hydration and prefetch utilities

--- apps/web/src/app/project-setup/layout.tsx
+++ apps/web/src/app/project-setup/layout.tsx
@@ -1,15 +1,20 @@
 import type { ReactNode } from "react";
 import { SetupHeader } from "@/components/layout/setup-header";
+import { HydrateClient, prefetch } from "@/orpc";
+import { orpc } from "@/utils/orpc";
 
-export default function ProjectSetupLayout({
+export default async function ProjectSetupLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  await prefetch(orpc.user.me.queryOptions());
   return (
-    <div className="flex h-svh flex-col items-center justify-center">
-      <SetupHeader />
-      {children}
-    </div>
+    <HydrateClient>
+      <div className="flex h-svh flex-col items-center justify-center">
+        <SetupHeader />
+        {children}
+      </div>
+    </HydrateClient>
   );
 }
--- apps/web/src/app/project-setup/page.tsx
+++ apps/web/src/app/project-setup/page.tsx
@@ -1,53 +1,12 @@
-"use client";
+import { ProjectSetup } from "@/components/setup/project-setup";
+import { verifySession } from "@/lib/dal";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import {
-  ProjectSetupForm,
-  type ProjectSetupSchema,
-  projectSetupSchema,
-} from "@/components/setup/project-setup-form";
-import { Button } from "@/components/ui/button";
-
-export default function ProjectSetupPage() {
-  const form = useForm<ProjectSetupSchema>({
-    resolver: zodResolver(projectSetupSchema),
-    defaultValues: {
-      projectName: "",
-      projectURL: "",
-    },
-  });
-
-  function onSubmit(data: ProjectSetupSchema) {
-    console.log("Project Setup Data:", data);
-
-    form.reset();
-  }
+export default async function ProjectSetupPage() {
+  await verifySession();
 
   return (
     <div className="w-full max-w-sm px-3 text-center sm:max-w-md">
-      <div className="space-y-8">
-        <div className="mx-auto max-w-sm space-y-4">
-          <h1 className="font-medium text-xl">Create your project</h1>
-          <p className="text-muted-foreground text-sm">
-            Projects are shared environments where you can collaborate with your
-            team.
-          </p>
-        </div>
-        <div className="w-full rounded-md bg-background-200 px-5 py-6 shadow-xl ring-1 ring-gray-200">
-          <form id="project-setup-form" onSubmit={form.handleSubmit(onSubmit)}>
-            <ProjectSetupForm form={form} />
-          </form>
-        </div>
-        <Button
-          className="h-11 w-full sm:w-sm"
-          form="project-setup-form"
-          size={"lg"}
-          type="submit"
-        >
-          Continue
-        </Button>
-      </div>
+      <ProjectSetup />
     </div>
   );
 }
