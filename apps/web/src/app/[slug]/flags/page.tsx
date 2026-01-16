49aca839433b3ff077de1fb05b8d89f73784005c
Rajdeep Das <rajdeepds626@gmail.com>
2026-01-16 06:27:42 +0000

    web: add flags list table

--- apps/web/src/app/[slug]/flags/page.tsx
+++ apps/web/src/app/[slug]/flags/page.tsx
@@ -1,10 +1,30 @@
 import Link from "next/link";
+import { columns, type Flag } from "@/components/flags/flags-column";
+import { ListTable } from "@/components/layout/list-table";
 import { PageHeader } from "@/components/layout/page-header";
 import { ScrollableContainer } from "@/components/layout/scrollable-container";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
-export default function FlagsPage() {
+async function getData(): Promise<Flag[]> {
+  return [
+    {
+      id: "728ed52f",
+      name: "New Feature Flag",
+      key: "new-feature-flag",
+      status: false,
+    },
+    {
+      id: "cmjinxj5t00056hkmqukxkd8c",
+      name: "Dashboard Flag",
+      key: "dashboard-flag",
+      status: true,
+    },
+  ];
+}
+
+export default async function FlagsPage() {
+  const data = await getData();
   return (
     <div>
       <PageHeader
@@ -25,7 +45,9 @@ export default function FlagsPage() {
           </Link>
         }
       />
-      <ScrollableContainer>hello</ScrollableContainer>
+      <ScrollableContainer>
+        <ListTable columns={columns} data={data} />
+      </ScrollableContainer>
     </div>
   );
 }
---
+++ apps/web/src/components/flags/flags-column.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import type { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+
+export interface Flag {
+  id: string;
+  name: string;
+  key: string;
+  status: Boolean;
+}
+
+export const columns: ColumnDef<Flag>[] = [
+  {
+    accessorKey: "name",
+    header: () => <div className="pl-2">Name</div>,
+    cell: ({ row }) => (
+      <Link className="pl-2" href={{ pathname: `/flags/${row.original.key}` }}>
+        {row.getValue("name") as string}
+      </Link>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const isDefault = row.getValue("status") as boolean;
+      return <span>{isDefault ? "Enabled" : "Disabled"}</span>;
+    },
+  },
+];
